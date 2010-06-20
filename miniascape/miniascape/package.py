4c0deb5491bc2d7e8b4c20efc016d063f0be58ac
Satoru SATOH <satoru.satoh@gmail.com>
2010-06-21 02:40:14 +0900

    Started to implement pack/repack command

--- miniascape/miniascape/package.py
+++ miniascape/miniascape/package.py
@@ -31,54 +31,108 @@ import re
 import shutil
 import sys
 
+import logging
+import os
+import re
 
-"""
-$domain.xml
-$domain.images
-#if $getVar('variant', '')
-$domain.name
-$variant
-$timestamp
-"""
+import miniascape as m
+from miniascape.globals import PKG_CONFIG_PATH
 
 
 
-def do_repackage_setup(domain_xml, builddir):
-    """setup packaging dir and files to re-package vm images.
+class Workflow(object):
 
-    @domain_xml  Domain XML file path
-    @builddir    
-    """
-    if domain_xml:
-        domxml_content = open(domain_xml).read()
-    else:
-        domxml_content = get_domain_xml(domain_name)
+    def __init__(self, name, variant="", topdir="", pkg_config_path=PKG_CONFIG_PATH):
+        """
+        @name         VM's name.
+        @pkg_config_path  Common config file path
+        """
+        self.config_path = pkg_config_path
+        self.config = m.config.getInstance(pkg_config_path)
 
-    domain_images = domain_image_paths(domxml_content)
+        self.name = name
 
-    substs = {
-        '%%DOMAIN_NAME%%': domain_name,
-        '%%DOMAIN_VARIANT%%': domain_variant,
-        '%%DOMAIN_IMAGES%%': ' '.join((os.path.basename(p) for p in domain_images))
-    }
+        if variant:
+            self.variant = variant
+        else:
+            self.variant = self.config.pack.variant
 
-    createdir(builddir)
-    createdir(os.path.join(builddir, M4DIR))
+        self.package_name = "vm-%s-%s" % (self.name, self.variant)
+        self.package_version = self.config.pack.version
 
-    open("%s/%s.xml" % (builddir, domain_name), 'w').write(domxml_content)
+        if topdir:
+            self.workdir = os.path.join(prefix, "%s-%s" % (self.package_name, self.package_version))
+        else:
+            self.workdir = "./%s-%s" % (self.package_name, self.package_version)
 
-    copyfile(os.path.join(libpath, AUXDIR, 'rpm.mk'), os.path.join(builddir, AUXDIR))
-    copyfile(os.path.join(libpath, M4DIR, 'qemu.m4'), os.path.join(builddir, M4DIR))
-    copyfile(os.path.join(libpath, M4DIR, 'rpm.m4'), os.path.join(builddir, M4DIR))
-    copyfile(os.path.join(libpath, 'repackage', 'Makefile.am.in'), os.path.join(builddir, 'Makefile.am'))
-    copyfile(os.path.join(libpath, 'repackage', 'README.in'), builddir)
-    copyfile(os.path.join(libpath, 'repackage', 'vm-image.spec.in'), \
-        os.path.join(builddir, 'vm-%s-%s.spec.in' % (domain_name, domain_variant)))
+    def setup_workdir(self, *args, **kwargs):
+        """
+        @throw OSError, etc.
+        """
+        os.makedirs(self.workdir, 0700)
 
-    substfile(os.path.join(libpath, 'repackage', 'configure.ac.in'), os.path.join(builddir, 'configure.ac'), substs)
+    def setup_buildfiles(self, *args, **kwargs):
+        pass
 
-    for image in domain_images:
-        copyfile(image, builddir)
+    def setup_data(self, *args, **kwargs):
+        pass
+
+    def setup(self, *args, **kwargs):
+        self.setup_workdir(*args, **kwargs)
+        self.setup_data(*args, **kwargs)
+        self.setup_buildfiles(*args, **kwargs)
+
+    def build_main(self, *args, **kwargs):
+        pass
+
+    def build_archive(self, *args, **kwargs):
+        pass
+
+    def build_src_rpm(self, *args, **kwargs):
+        pass
+
+    def build_bin_rpm(self, *args, **kwargs):
+        pass
+
+    def build(self, *args, **kwargs):
+        self.build_main(*args, **kwargs)
+        self.build_archive(*args, **kwargs)
+        self.build_src_rpm(*args, **kwargs)
+
+    def template_output(self, tmpl):
+        return os.path.join(self.workdir, tmpl.replace('.tmpl',''))
+
+
+
+class RepackWorkflow(Workflow):
+
+    def setup_buildfiles(self, *args, **kwargs):
+        tmpls = ['Makefile.am.tmpl', 'README.tmpl', 'configure.ac.tmpl',  'vm.spec.in.tmpl']
+        tmpls = [os.path.join(self.config.dirs.templatesdir, "repack", t) for t in tmpls]
+
+        for t in tmpls:
+            m.utils.compile_template(t, self.template_output(t), self.domain)
+
+        os.makedirs(os.path.join(self.workdir, 'aux'))
+        os.makedirs(os.path.join(self.workdir, 'aux', 'm4'))
+
+        m.utils.copyfile(os.path.join(self.config.dirs.auxdir, 'rpm.mk'), self.workdir, force=True)
+
+        for mf in ('libvirt.m4', 'package.m4', 'python.m4', 'qemu.m4', 'rpm.m4'):
+            m.utils.copyfile(os.path.join(self.config.dirs.m4dir, mf), self.workdir, force=True)
+
+    def setup_data(self, *args, **kwargs):
+        self.domain = m.virt.LibvirtDomain(self.name, self.config_path)
+
+        self.domain.parse()
+
+        xml = re.sub(r'<uuid>.+</uuid>\n', '', str(self.domain))
+        open(os.path.join(self.workdir, "%s.xml" % self.name), "w").write(xml)
+
+        for img in self.images + self.base_images:
+            m.utils.copyfile(img, os.path.join(self.workdir, os.path.basename(img)))
+
+
 
 
 def do_package_setup(libpath, domain_name, domain_variant, builddir, *args):
