ab2af1126f8d90bb50a5dfd16260d59bf1f126b6
Satoru SATOH <satoru.satoh@gmail.com>
2010-10-27 04:03:35 +0900

    removed (unused any more)

--- build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-set -e
-set -x
-
-for d in miniascape; do
-    autoreconf -vfi
-    ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --sharedstatedir=/var/lib
-    make
-done
