90e5dff51a209b5dd29c9c0bbcdf3e72e549e736
Satoru SATOH <satoru.satoh@gmail.com>
2011-01-21 18:45:31 +0900

    fixed up some failures in network related variable handlings

--- core/devel/network.mk
+++ core/devel/network.mk
@@ -7,26 +7,27 @@ $(error You must specify miniascape_NET_NETWORK_1, that is, single network inter
 endif
 
 miniascape_NET_1_MODEL	?= $(miniascape_NET_MODEL)
-miniascape_MAC_1	?= $(miniascape_NET_MAC)
-network_opts	= --network=$(miniascape_NET_NETWORK_1),model=$(miniascape_NET_1_MODEL),mac=$(miniascape_MAC_1)
-
+mac_1_opt	= $(shell test "x$(miniascape_MAC_1)" = "xRANDOM" -o "x$(miniascape_MAC_1)" = "x" && echo "" || echo ",mac=$(miniascape_MAC_1)")
+network_opts	= --network=$(miniascape_NET_NETWORK_1),model=$(miniascape_NET_1_MODEL)$(mac_1_opt)
 
 ifneq ($(miniascape_NET_NETWORK_2),)
 miniascape_NET_2_MODEL	?= $(miniascape_NET_MODEL)
-miniascape_MAC_2	?= $(miniascape_NET_MAC)
-network_opts += --network=$(miniascape_NET_NETWORK_2),model=$(miniascape_NET_2_MODEL),mac=$(miniascape_MAC_2)
+mac_2_opt	= $(shell test "x$(miniascape_MAC_2)" = "xRANDOM" -o "x$(miniascape_MAC_2)" = "x" && echo "" || echo ",mac=$(miniascape_MAC_2)")
+network_opts += --network=$(miniascape_NET_NETWORK_2),model=$(miniascape_NET_2_MODEL)$(mac_2_opt)
 endif
 
 ifneq ($(miniascape_NET_NETWORK_3),)
 miniascape_NET_3_MODEL	?= $(miniascape_NET_MODEL)
 miniascape_MAC_3	?= $(miniascape_NET_MAC)
-network_opts += --network=$(miniascape_NET_NETWORK_3),model=$(miniascape_NET_3_MODEL),mac=$(miniascape_MAC_3)
+mac_3_opt	= $(shell test "x$(miniascape_MAC_3)" = "xRANDOM" -o "x$(miniascape_MAC_3)" = "x" && echo "" || echo ",mac=$(miniascape_MAC_3)")
+network_opts += --network=$(miniascape_NET_NETWORK_3),model=$(miniascape_NET_3_MODEL)$(mac_3_opt)
 endif
 
 ifneq ($(miniascape_NET_NETWORK_4),)
 miniascape_NET_4_MODEL	?= $(miniascape_NET_MODEL)
 miniascape_MAC_4	?= $(miniascape_NET_MAC)
-network_opts += --network=$(miniascape_NET_NETWORK_4),model=$(miniascape_NET_4_MODEL),mac=$(miniascape_MAC_4)
+mac_4_opt	= $(shell test "x$(miniascape_MAC_4)" = "xRANDOM" -o "x$(miniascape_MAC_4)" = "x" && echo "" || echo ",mac=$(miniascape_MAC_4)")
+network_opts += --network=$(miniascape_NET_NETWORK_4),model=$(miniascape_NET_4_MODEL)$(mac_4_opt)
 endif
 
 # vim:set ft=make ai si sm:
