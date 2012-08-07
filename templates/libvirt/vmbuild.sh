1f53022a22b8253972d4eb062920fd343ea79a7b
Satoru SATOH <satoru.satoh@gmail.com>
2012-08-07 09:34:17 +0900

    added

---
+++ templates/libvirt/vmbuild.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+#
+# see also virt-install(1)
+#
+set -e
+set -x
+ks_path=${0%/*}/{{ kscfg }} 
+
+{%- macro net_option(nic) -%}
+network={{ nic.network|default('network') }},model={{ nic.model|default('virtio') }},mac={{ nic.mac|default('RANDOM') }}
+{%- endmacro -%}
+{% macro disk_option(disk) -%}
+pool={{ disk.src|default('default') }},format={{ disk.format|default('qcow2') }},cache={{ disk.cache|default('none') }},size={{ disk.size|default('5') }}
+{%- endmacro %}
+
+virt-install \
+{{ virtinst.basic_options }} \
+--name={% if name is defined %}{{ name }}{% else %}{{ hostname }}{% endif %} \
+--connect={{ virtinst.connect }} \
+--wait={{ virtinst.waittime }} \
+--ram={{ virtinst.ram }} \
+--arch={{ virtinst.arch }} \
+--vcpus={{ virtinst.vcpus }} \
+--graphics {{ virtinst.graphics }} \
+--os-type={{ virtinst.os_type }} \
+--os-variant={{ virtinst.os_variant }} \
+--location={{ virtinst.location }} \
+--initrd-inject=${ks_path} \
+{% for disk in disks %}--disk "{{ disk_option(disk) }}" {% endfor %} \
+{% for nic in interfaces %}--network "{{ net_option(nic) }}" {% endfor %} \
+--extra-args="ks=file:/{{ kscfg }} ksdevice={{ ksdevice }} {{ virtinst.extra_args|default('') }}"
+
