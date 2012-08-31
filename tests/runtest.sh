a46238e248a676ef7162e44ce3fd3a028df6e69b
Satoru SATOH <satoru.satoh@gmail.com>
2012-08-31 21:14:07 +0900

    added helper script for unit/doctests

---
+++ tests/runtest.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+set -e
+
+testsdir=${0%/*}
+topdir=../$testsdir
+
+which pep8 2>&1 > /dev/null && check_with_pep8=1 || check_with_pep8=0
+
+if test $# -gt 0; then
+    test $check_with_pep8 = 1 && (for x in $@; do pep8 ${x%%:*}; done) || :
+    PYTHONPATH=$topdir nosetests -c $testsdir/nose.cfg $@
+else
+    PYTHONPATH=$topdir nosetests -c $testsdir/nose.cfg -w $testsdir
+fi
