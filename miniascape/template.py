9cbbc1ca8e6bc36ccdcf0277960225c83a69174f
Satoru SATOH <satoru.satoh@gmail.com>
2012-09-02 10:03:37 +0900

    new function 'renderto' to render template to output in miniascape.template

--- miniascape/template.py
+++ miniascape/template.py
@@ -14,6 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+import jinja2_cui.render as R
+
+import os.path
+import os
 
 
 def mk_tmpl_cmd(tpaths, configs, output, tmpl):
@@ -36,6 +40,16 @@ def mk_tmpl_cmd(tpaths, configs, output, tmpl):
     )
 
     return "%(prog)s %(topts)s %(copts)s -o %(output)s %(tmpl)s" % params
+
+
+def renderto(tpaths, config, tmpl, output):
+    outdir = os.path.dirname(output)
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    c = R.render(tmpl, config, tpaths)
+    open(output, "w").write(c)
 
 
 # vim:sw=4:ts=4:et:
