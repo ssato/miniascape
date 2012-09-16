7cdd360df72fb2761aae314196d875b1b26ad480
Satoru SATOH <satoru.satoh@gmail.com>
2012-09-17 00:05:31 +0900

    added a few hacks for rhbz#857424

--- templates/libvirt/vmbuild.sh
+++ templates/libvirt/vmbuild.sh
@@ -4,12 +4,31 @@
 set -ex
 test $# -gt 0 && ks_path=$1 || ks_path=${0%/*}/ks.cfg
 kscfg=${ks_path##*/}
+location={{ virtinst.location }}
 
+# Create storage volumes by virsh or qemu instead of virt-install as needed.
+# see also: rhbz#857424
+{%     for disk in disks %}{% if not loop.first -%}
+{%-         if disk.path is defined -%}
+test -f {{ disk.path }} || \
+    qemu-img create -f {{ disk.format|default('qcow2') }} {{ disk.path }} {{ disk.size|default('5') }}
+{%-         else -%}
+virsh vol-key {{ disk.vol }} {{ disk.pool|default('default') }} | \
+    virsh vol-create-as {{ disk.pool|default('default') }} {{ disk.vol }} {{ disk.size|default('5') }}GiB --format {{ disk.format|default('qcow2') }}
+{%-         endif -%}
+{%-    endif %}{% endfor %}
 {%- macro net_option(nic) -%}
 network={{ nic.network|default('network') }},model={{ nic.model|default('virtio') }},mac={{ nic.mac|default('RANDOM') }}
 {%- endmacro -%}
-{% macro disk_option(disk) -%}
-{{ disk.type|default('pool') }}={{ disk.src|default('default') }},format={{ disk.format|default('qcow2') }},cache={{ disk.cache|default('none') }},size={{ disk.size|default('5') }}{% if disk.perms is defined %},perms={{ disk.perms }}{% endif %}
+{%- macro disk_option(disk, first=true) -%}
+{%-     if disk.pool is defined -%}
+{%-         if disk.vol is defined %}vol={{ disk.pool }}/{{ disk.vol }}{% else %}pool={{ disk.pool }}{% endif -%}
+{%-     else %}path={{ disk.path }}
+{%-     endif -%}
+,format={{ disk.format|default('qcow2') }},cache={{ disk.cache|default('none') }}
+{%-     if first %},size={{ disk.size|default('5') }}{% endif -%}{# use pre-built volume if not first disk #}
+{%-     if disk.perms is defined %},perms={{ disk.perms }}{% endif -%}
+{%-     if disk.bus is defined %},bus={{ disk.bus }}{% endif -%}
 {%- endmacro %}
 
 virt-install \
@@ -23,8 +42,13 @@ virt-install \
 --graphics {{ virtinst.graphics }} \
 --os-type={{ virtinst.os_type }} \
 --os-variant={{ virtinst.os_variant }} \
---location={{ virtinst.location }} \
+--location=${location} \
 --initrd-inject=${ks_path} \
-{% for disk in disks %}{% if loop.first %}--disk "{{ disk_option(disk) }}" {% endif %}{% endfor %} \
-{% for nic in interfaces %}--network "{{ net_option(nic) }}" {% endfor %} \
+{% for disk in disks -%}
+{%-    if loop.first %}--disk {{ disk_option(disk) }} \
+{%     else %}--disk {{ disk_option(disk, false) }} \
+{%-    endif -%}{% endfor %}
+{% for nic in interfaces -%}--network {{ net_option(nic) }} \
+{%- endfor %}
 --extra-args="ks=file:/${kscfg} ksdevice={{ ksdevice|default('eth0') }} {{ virtinst.extra_args|default('') }}"
+
