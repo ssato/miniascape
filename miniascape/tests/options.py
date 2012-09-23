ee929bafde028636116c14f7fa19ae092c9932cf
Satoru SATOH <satoru.satoh@gmail.com>
2012-09-23 23:29:17 +0900

    added test cases for miniascape.options

---
+++ miniascape/tests/options.py
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2012 Satoru SATOH <ssato@redhat.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import miniascape.options as O
+import miniascape.globals as G
+
+import optparse
+import unittest
+
+
+class Test_functions(unittest.TestCase):
+
+    def test_option_parser__00_wo_args(self):
+        p = O.option_parser()
+        self.assertTrue(isinstance(p, optparse.OptionParser))
+        self.assertEquals(p.defaults, O.M_DEFAULTS)
+
+    def test_tweak_tmpldir(self):
+        p = O.option_parser()
+        (options, args) = p.parse_args([])
+
+        self.assertEquals(options.tmpldir, [])
+
+        options = O.tweak_tmpldir(options)
+        self.assertEquals(options.tmpldir, [G.M_TMPL_DIR])
+
+
+# vim:sw=4:ts=4:et:
