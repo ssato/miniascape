a0eff25a6559b755796396b97e58eb40ebaaf95e
Satoru SATOH <satoru.satoh@gmail.com>
2012-10-11 15:29:04 +0900

    parameterized cluster name, clvm vg and lv, gfs fs

--- templates/autoinstall.d/data/rhel-6-cluster-gfs/init_gfs.sh
+++ templates/autoinstall.d/data/rhel-6-cluster-gfs/init_gfs.sh
@@ -23,8 +23,7 @@ fi
 # Enable CLVM lock, system service (clvmd) and start it if not:
 grep -q -E '^[ ]+locking_type = 3' /etc/lvm/lvm.conf 2>/dev/null || \
   lvmconf --enable-cluster
-/sbin/chkconfig --list clvmd | grep -q "3:on" 2>/dev/null || \
-  /sbin/chkconfig clvmd on
+/sbin/chkconfig --list clvmd | grep -q "3:on" 2>/dev/null || /sbin/chkconfig clvmd on
 /sbin/service clvmd status || /sbin/service clvmd start
 
 
@@ -35,14 +34,14 @@ diskdev=$1
 vg=$2
 lv=$3
 test "x$diskdev" = "x" && diskdev=/dev/${disk1:?}2
-test "x$vg" = "x" && vg=clustered_vg
-test "x$lv" = "x" && lv=clustered_lv
+test "x$vg" = "x" && vg={{ cluster.vol.vg }}clustered_vg
+test "x$lv" = "x" && lv={{ cluster.vol.lv }}clustered_lv
 
 # Create pv, vg, lv if not exist:
 pvscan -s | grep -q $diskdev 2>/dev/null || pvcreate $diskdev
 vgdisplay -s --nosuffix | grep -q "\"$vg\"" 2>/dev/null || vgcreate -cy $vg $diskdev
 lvscan -a | grep -q /dev/$vg/$lv 2>/dev/null || lvcreate -n $lv -l 100%VG $vg
 
-mkfs.gfs2 -t DEMO_GFS_CLUSTER:gfs01 -j {{ cluster.nodes|length }} -J 64 /dev/clusteredvg/lv_gfs
+mkfs.gfs2 -t {{ cluster.name }}:{{ cluster.vol.fs }} -j {{ cluster.nodes|length }} -J 64 /dev/$vg/$lv
 
 # vim:sw=2:ts=2:et:
