e1bd082d971522186888bd08998c830252190839
Satoru SATOH <satoru.satoh@gmail.com>
2012-10-17 18:49:45 +0900

    fixed parameter names

--- templates/autoinstall.d/data/lvs/setup_nat.sh
+++ templates/autoinstall.d/data/lvs/setup_nat.sh
@@ -12,12 +12,12 @@ EOF
 /sbin/iptables save
 cp /etc/sysconfig/iptables /etc/sysconfig/iptables.save
 # Firewall marks for HTTP, HTTPS
-/sbin/iptables -t mangle -A PREROUTING -p tcp -d {{ lvs.nat.private.ip.addr }}/{{ lvs.nat.private.ip.maskbit }} -m multiport --dports 80,443 -j MARK --set-mark 80
+/sbin/iptables -t mangle -A PREROUTING -p tcp -d {{ lvs.private.ip.addr }}/{{ lvs.private.ip.maskbit }} -m multiport --dports 80,443 -j MARK --set-mark 80
 # Rules for FTP active connections in LVS:
 # Allows this LVS router to accept outgoing FTP connections from real servers
 # that IPVS does not know about (http://red.ht/R6HsJn):
-/sbin/iptables -t nat -A POSTROUTING -p tcp -s {{ lvs.nat.private.ip.network }}/{{ lvs.nat.private.ip.maskbit }} --sport 20 -j MASQUERADE
+/sbin/iptables -t nat -A POSTROUTING -p tcp -s {{ lvs.private.ip.network }}/{{ lvs.private.ip.maskbit }} --sport 20 -j MASQUERADE
 # Rules for FTP passive connections in LVS (http://red.ht/OBrlF3):
-/sbin/iptables -t mangle -A PREROUTING -p tcp -d {{ lvs.nat.private.ip.addr }}/{{ lvs.nat.private.ip.maskbit }} --dport 21 -j MARK --set-mark 21
-/sbin/iptables -t mangle -A PREROUTING -p tcp -d {{ lvs.nat.private.ip.addr }}/{{ lvs.nat.private.ip.maskbit }} --dport 10000:20000 -j MARK --set-mark 21 
+/sbin/iptables -t mangle -A PREROUTING -p tcp -d {{ lvs.private.ip.addr }}/{{ lvs.private.ip.maskbit }} --dport 21 -j MARK --set-mark 21
+/sbin/iptables -t mangle -A PREROUTING -p tcp -d {{ lvs.private.ip.addr }}/{{ lvs.private.ip.maskbit }} --dport 10000:20000 -j MARK --set-mark 21 
 /sbin/iptables save
