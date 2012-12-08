57b639d33036d6074b83b31a9d1a64d8b829517d
Satoru SATOH <satoru.satoh@gmail.com>
2012-12-08 22:25:27 +0900

    fixed bugs: check cman services started, key file for fence_xvm exists, only run on primary node, etc.

--- templates/autoinstall.d/data/rhel-6-cluster-gfs/init_gfs.sh
+++ templates/autoinstall.d/data/rhel-6-cluster-gfs/init_gfs.sh
@@ -1,33 +1,15 @@
 #! /bin/bash
-set -e -x -v
-exec 1> ${0%/*}/${0##*/}.log 2>&1
-
-cconf=/etc/cluster/cluster.conf
-if test ! -f $cconf; then
-  echo "Cluster config '$1' not found. Aborting..."
-  exit 1
-else
-  if `/sbin/service cman status 2>/dev/null >/dev/null`; then
-    if `cman_tool status 2>/dev/null >/dev/null`; then
-      echo "Cluster looks working. Going forward..."
-    else
-      echo "Cluster does not look working. Check it before initializing GFS..."
-      exit 1
-    fi
-  else
-    echo "CMAN does not look working. Check cluster configuration before initializing GFS..."
-    exit 1
-  fi
-fi
-
-# Enable cman service
-/sbin/chkconfig --list cman | grep -q "3:on" 2>/dev/null || /sbin/chkconfig cman on
-
-# Enable CLVM lock, system service (clvmd) and start it if not:
-grep -q -E '^[ ]+locking_type = 3' /etc/lvm/lvm.conf 2>/dev/null || \
-  lvmconf --enable-cluster
-/sbin/chkconfig --list clvmd | grep -q "3:on" 2>/dev/null || /sbin/chkconfig clvmd on
-/sbin/service clvmd status || /sbin/service clvmd start
+set -e
+curdir=${0%/*}
+{% if cluster_init is defined and cluster_init %}
+check_cman=${curdir}/check_cman.sh
+start_clvmd=${curdir}/start_clvmd.sh
+{% for node in cluster.nodes %}
+{%   if loop.first %}bash $check_cman{% else %}ssh root@{{ node.name }} "bash $check_cman"{% endif %}
+{% endfor %}
+{% for node in cluster.nodes %}
+{%   if loop.first %}bash $start_clvmd{% else %}ssh root@{{ node.name }} "bash $start_clvmd"{% endif %}
+{% endfor %}
 {% import "snippets/pre_post.find_disk" as F -%}
 {{ F.find_disk_device(2) }}
 diskdev=$1
@@ -38,21 +20,12 @@ test "x$vg" = "x" && vg={{ cluster.vol.vg|default('gfs-vg-0') }}
 test "x$lv" = "x" && lv={{ cluster.vol.lv|default('gfs-lv-0') }}
 
 # Create pv, vg, lv if not exist:
-if `pvscan -s | grep -q $diskdev 2>/dev/null > /dev/null`; then
-  echo "It looks PV $diskdev already created. Skip this step."
-else
-  pvcreate $diskdev
-fi
-if `vgdisplay -s --nosuffix | grep -q "\"$vg\"" 2>/dev/null > /dev/null`; then
-  echo "It looks VG $vg already created. Skip this step."
-else
+pvscan -s | grep -q $diskdev 2>/dev/null > /dev/null || pvcreate $diskdev
+vgdisplay -s --nosuffix | grep -q "\"$vg\"" 2>/dev/null > /dev/null || \
   vgcreate -cy $vg $diskdev
-fi
-if `lvscan -a | grep -q /dev/$vg/$lv 2>/dev/null >/dev/null`; then
-  echo "It looks LV $lv already created. Skip this step."
-else
+lvscan -a | grep -q /dev/$vg/$lv 2>/dev/null >/dev/null || \
   lvcreate -n $lv -l 100%VG $vg && \
     mkfs.gfs2 -t {{ cluster.name }}:{{ cluster.vol.fs }} -j {{ cluster.nodes|length }} -J {{ cluster.vol.journal.size|default('64') }} /dev/$vg/$lv
-fi
-
+{%- else -%}
+echo "This script should be running on node #1"{% endif %}
 # vim:sw=2:ts=2:et:
