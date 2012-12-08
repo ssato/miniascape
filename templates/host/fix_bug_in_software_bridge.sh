43921d517e92d41a870d69ecd6ba630065ec56fe
Satoru SATOH <satoru.satoh@gmail.com>
2012-12-08 16:10:23 +0900

    set default bridge to make multicast communication worked

--- templates/host/fix_bug_in_software_bridge.sh
+++ templates/host/fix_bug_in_software_bridge.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 # Avoid a bug in linux kernel's software bridge bug in multicast_snooping:
-echo 0 > /sys/class/net/{{ bridge }}/bridge/multicast_snooping
+echo 0 > /sys/class/net/{{ bridge|default('virbr0') }}/bridge/multicast_snooping
