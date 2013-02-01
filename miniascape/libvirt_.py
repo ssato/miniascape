4dc3782057791aecc7a2a14797c47ae4d37b0b90
Satoru SATOH <satoru.satoh@gmail.com>
2013-02-01 16:39:55 +0900

    fixed module name to import

--- miniascape/libvirt_.py
+++ miniascape/libvirt_.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import miniascape.xml as X
+import miniascape.xml_ as X
 
 import libvirt
 import logging
