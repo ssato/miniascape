dd8712dbecc54a38029800214425a795586f1dd6
Satoru SATOH <satoru.satoh@gmail.com>
2013-02-13 11:23:04 +0900

    Switched template rendering module from python-jinja2-cui to python-jinja2-cli

--- miniascape/template.py
+++ miniascape/template.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import jinja2_cui.render as R
+import jinja2_cli.render as R
 
 
 def renderto(tpaths, config, tmpl, output, ask=False):
