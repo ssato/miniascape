b43612d4d76c69d0c993e461c63f67e75cef2949
Satoru SATOH <satoru.satoh@gmail.com>
2013-04-01 00:14:23 +0900

    set log level of anyconfig also

--- miniascape/globals.py
+++ miniascape/globals.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+import anyconfig
 import datetime
 import locale
 import logging
@@ -69,5 +70,6 @@ def set_loglevel(level):
     assert level in (0, 1, 2), "Invalid log level option was passed: " + level
     lvl = [logging.DEBUG, logging.INFO, logging.WARN][level]
     LOGGER.setLevel(lvl)
+    anyconfig.set_loglevel(lvl)
 
 # vim:sw=4:ts=4:et:
