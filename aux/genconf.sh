d43e34cbe616f29cce46045195c6d97691d94e68
Satoru SATOH <satoru.satoh@gmail.com>
2013-10-09 23:23:31 +0900

    make the script to generate configuration files to load multiple config files from given dir instead of just loading single config file

--- aux/genconf.sh
+++ aux/genconf.sh
@@ -3,4 +3,4 @@ curdir=${0%/*}
 topdir=${curdir}/../
 
 (cd ${topdir} && \
- PYTHONPATH=. python tools/miniascape c -t ./templates -w ./ -c ./default/src.d/00_base.yml)
+ PYTHONPATH=. python tools/miniascape c -t ./templates -w ./ -c ./default/src.d)
