5de1b3c6d8472a7c0a240655795114168b08622c
Satoru SATOH <satoru.satoh@gmail.com>
2013-10-16 19:20:28 +0900

    add -f (force) option to generate config files always

--- aux/genconf.sh
+++ aux/genconf.sh
@@ -3,4 +3,4 @@ curdir=${0%/*}
 topdir=${curdir}/../
 
 (cd ${topdir} && \
- PYTHONPATH=. python tools/miniascape c -t ./templates -w ./ -c ./default/src.d)
+ PYTHONPATH=. python tools/miniascape c -f -t ./templates -w ./ -c ./default/src.d)
