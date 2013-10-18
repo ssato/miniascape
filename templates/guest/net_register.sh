d03447bbcfebfeac942f26a9358380bd2500adea
Satoru SATOH <satoru.satoh@gmail.com>
2013-10-18 22:55:24 +0900

    fix a bug in templates/guest/net_register.sh that the path of the script to invoke is not set

--- templates/guest/net_register.sh
+++ templates/guest/net_register.sh
@@ -4,11 +4,11 @@
 
 # Use installed version of 'guest_network_register.sh' if exists:
 register_sh=/usr/libexec/miniascape/guest_network_register.sh
-test -x ${register_sh} || register_sh=${0%/*}/../../host/${register_sh}
+test -f ${register_sh} || register_sh=${0%/*}/../../host/${register_sh}
 
 {% for nic in interfaces -%}
 {%     if nic.mac is defined and nic.ip is defined and nic.fqdn is defined -%}
-bash {{ script }} -m {{ nic.mac }} -n {{ nic.network|default('default') }} {{ nic.fqdn }} {{ nic.ip }}
+bash ${register_sh} -m {{ nic.mac }} -n {{ nic.network|default('default') }} {{ nic.fqdn }} {{ nic.ip }}
 {%-    endif %}
 {% endfor %}
 
