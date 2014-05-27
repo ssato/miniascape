2d043fff7658b88a7b37f54529dab7dafae57f13
Satoru SATOH <satoru.satoh@gmail.com>
2014-05-27 11:11:59 +0900

    fix the path to generated conf files from ./[default/] to conf/[default]

--- aux/genconf.sh
+++ aux/genconf.sh
@@ -3,4 +3,4 @@ curdir=${0%/*}
 topdir=${curdir}/../
 
 (cd ${topdir} && \
- PYTHONPATH=. python tools/miniascape c -f -t ./templates -w ./ -c ./conf/default/src.d)
+ PYTHONPATH=. python tools/miniascape c -f -t ./templates -w ./conf -c ./conf/default/src.d)
