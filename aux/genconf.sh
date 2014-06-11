93cb04fcdf12178d407679d8bc309553404cec3e
Satoru SATOH <satoru.satoh@gmail.com>
2014-06-11 16:04:15 +0900

    removed as it was unnecessary because 'configure' and 'compile' (generate) process was migrated into one step 'build'

--- aux/genconf.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-curdir=${0%/*}
-topdir=${curdir}/../
-
-(cd ${topdir} && \
-for d in ./conf/*/src.d; do \
-  PYTHONPATH=. python tools/miniascape c -f -t ./templates -w ./conf -c $d; \
-done)
