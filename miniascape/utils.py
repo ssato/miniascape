06333b233ab84ed6f229eea392d6d6faa0fa7f34
Satoru SATOH <satoru.satoh@gmail.com>
2014-06-11 16:23:30 +0900

    removed runtime dependency to anyconfig in miniascape.utils

--- miniascape/utils.py
+++ miniascape/utils.py
@@ -18,7 +18,6 @@
 from miniascape.globals import LOGGER as logging
 from miniascape.memoize import memoize
 
-import anyconfig
 import glob
 import itertools
 import os.path
@@ -36,6 +35,12 @@ except AttributeError:
                 yield element
 
     chain_from_iterable = _from_iterable
+
+try:
+    from anyconfig import is_mergeabledict_or_dict
+except ImportError:
+    def is_mergeabledict_or_dict(x):
+        return isinstance(x, dict)
 
 
 def concat(xss):
@@ -153,11 +158,11 @@ def walk(x, path=None, path_sep='.', hook=noop):
     :param path: Current 'path'
     :param path_sep: Path separator
     """
-    if anyconfig.is_mergeabledict_or_dict(x):
+    if is_mergeabledict_or_dict(x):
         for k, v in x.iteritems():
             curpath = k if path is None else "%s%s%s" % (path, path_sep, k)
 
-            if anyconfig.is_mergeabledict_or_dict(v):
+            if is_mergeabledict_or_dict(v):
                 for path_child, v_child, _d in walk(v, curpath, path_sep,
                                                     hook):
                     yield hook(path_child, v_child, v)
