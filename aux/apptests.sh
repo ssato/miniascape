62d664c228a95e1fe98e79e6f12276e08a3771d4
Satoru SATOH <satoru.satoh@gmail.com>
2014-07-25 19:02:35 +0900

    make some of the output ks.cfg files validated w/ using ksvalidator

--- aux/apptests.sh
+++ aux/apptests.sh
@@ -5,13 +5,20 @@ curdir=${0%/*}
 topdir=${curdir}/../
 
 workdir=$(mktemp -d)
+cleanup () { rm -rf ${workdir}; }
 
-trap "rm -rf ${workdir}" INT TERM EXIT
+trap cleanup INT TERM
 
 for site in default openstack rhui; do
   PYTHONPATH=${topdir} python miniascape/cli.py b \
     -t ${topdir}/templates -s ${site} \
     -C "conf/${site}/\*.yml" -w ${workdir}
 done
+
+# check w/ ksvalidator:
+for f in ${workdir}/guests.d/rhel-5-*/ks.cfg; do ksvalidator -e -v RHEL5 $f; done
+for f in ${workdir}/guests.d/{rhel-6-,jboss}*/ks.cfg; do ksvalidator -e -v RHEL6 $f; done
+
+cleanup
 
 # vim:sw=2:ts=2:et:
