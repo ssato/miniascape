43ebc0324eb6e9f779264a2d818f13e03b04f9b5
Satoru SATOH <satoru.satoh@gmail.com>
2014-07-25 18:48:12 +0900

    create workdir dynamically in application tests

--- aux/apptests.sh
+++ aux/apptests.sh
@@ -4,10 +4,14 @@ set -ex
 curdir=${0%/*}
 topdir=${curdir}/../
 
+workdir=$(mktemp -d)
+
+trap "rm -rf ${workdir}" INT TERM EXIT
+
 for site in default openstack rhui; do
   PYTHONPATH=${topdir} python miniascape/cli.py b \
     -t ${topdir}/templates -s ${site} \
-    -C "conf/${site}/\*.yml" -w /tmp/w
+    -C "conf/${site}/\*.yml" -w ${workdir}
 done
 
 # vim:sw=2:ts=2:et:
