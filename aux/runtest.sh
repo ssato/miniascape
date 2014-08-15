0b08c3e9029a115ba4df81e47f6bb1016e8ab267
Satoru SATOH <satoru.satoh@gmail.com>
2014-08-15 12:38:00 +0900

    update unit tests runner script, copied from python-anyconfig

--- aux/runtest.sh
+++ aux/runtest.sh
@@ -3,16 +3,40 @@ set -e
 
 curdir=${0%/*}
 topdir=${curdir}/../
+nosetests_opts="-c ${curdir}/nose.cfg"
+nprocs=$(echo ${NOSE_PROCESSES})
 
 if `env | grep -q 'WITH_COVERAGE' 2>/dev/null`; then
-    coverage_opts="--with-coverage --cover-tests --cover-inclusive"
+    nosetests_opts="${nosetests_opts} --with-coverage --cover-tests"
+    nprocs=0  # It seems that coverage does not like parallel tests.
 fi
 
+if test "x${nprocs}" != "x0" ; then
+    if test -f /proc/cpuinfo; then
+        nprocs=$(sed -n '/^processor.*/p' /proc/cpuinfo | wc -l)
+        if test ${nprocs} -gt 0; then
+            nosetests_opts="${nosetests_opts} --processes=${nprocs}"
+        fi
+    fi
+fi
+
+function _pylint () {
+    pylint --rcfile=$curdir/pylintrc --errors-only $@
+}
+
 which pep8 2>&1 > /dev/null && check_with_pep8=1 || check_with_pep8=0
+which flake8 2>&1 > /dev/null && { check_with_pep8=0; check_with_flake8=1;} || check_with_flake8=0
+which pylint 2>&1 > /dev/null && check_with_pylint=1 || check_with_pylint=0
 
 if test $# -gt 0; then
-    test $check_with_pep8 = 1 && (for x in $@; do pep8 ${x%%:*}; done) || :
-    PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg ${coverage_opts} $@
+    if test $check_with_pep8 = 1; then
+        for x in $@; do pep8 ${x%%:*}; done
+    fi
+    test $check_with_flake8 = 1 && flake8 $@
+    if test $check_with_pylint = 1; then
+        for x in $@; do _pylint ${x%%:*}; done
+    fi
+    PYTHONPATH=$topdir nosetests ${nosetests_opts} $@
 else
     # Find out python package dir and run tests for .py files under it.
     for d in ${topdir}/*; do
@@ -21,12 +45,15 @@ else
 
             for f in $(find ${pypkgdir} -name '*.py'); do
                 echo "[Info] Check $f..."
-                test $check_with_pep8 = 1 && pep8 $f || :
-                PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg \
-                        ${coverage_opts} $f
+                if test $check_with_pep8 = 1; then pep8 $f; fi
+                if test $check_with_pylint = 1; then _pylint $f; fi
             done
 
             break
         fi
     done
+    PYTHONPATH=$topdir nosetests ${nosetests_opts} --all-modules
+    test $check_with_flake8 = 1 && flake8 ${topdir}
 fi
+
+# vim:sw=4:ts=4:et:
