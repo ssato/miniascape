57571bc4b594db6a4c3b98561c73756982769ed1
Satoru SATOH <satoru.satoh@gmail.com>
2014-08-26 20:20:28 +0900

    override base configurations to rebase RHEL version: virtinst.{os_variant, location}

--- tests/app0.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-curdir=${0%/*}
-topdir=${curdir}/../
-
-workdir=$(mktemp -d /tmp/miniascape-XXXXXXXXXX)
-
-(cd ${topdir} && \
- PYTHONPATH=. python tools/miniascape ge -t ./templates -w ${workdir} -c ./default)
