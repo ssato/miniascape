d0d2d37f9fcd171b97fbf67b2c67963cedc345a4
Satoru SATOH <satoru.satoh@gmail.com>
2015-01-31 22:50:00 +0900

    enable automatic satellite sync if swithing to online mode in satellte 5.x

--- templates/autoinstall.d/data/satellite/switch-to-online-mode.sh
+++ templates/autoinstall.d/data/satellite/switch-to-online-mode.sh
@@ -1,12 +1,15 @@
 #! /bin/sh
-set -e
-set -x
+set -ex
 
 conf=/etc/rhn/rhn.conf
 
 grep '^disconnected=0' ${conf} > /dev/null 2> /dev/null || \
-sed -i.org \
+sed -i.save \
   -e 's/\(.*rhn_parent = \).*/\1satellite.rhn.redhat.com/' \
   -e 's/\(disconnected=\)1/\10/' ${conf}
 
-exit \$?
+# Satellite 5.7 Installation Guide, 12.5. Automating Synchronization:
+# http://red.ht/1zI6UPd
+cat << EOF >> /var/spool/cron/root
+0 1 * * * perl -le 'sleep rand 9000' && satellite-sync --email >/dev/null 2>/dev/null
+EOF
