728bbf42bd5d846b6065d2b13001bf48c3af303a
Satoru SATOH <satoru.satoh@gmail.com>
2015-03-02 12:35:30 +0900

    enable ks.cfg validation test for satellite6

--- pkg/apptests.sh
+++ pkg/apptests.sh
@@ -17,7 +17,7 @@ done
 # check w/ ksvalidator:
 for f in ${workdir}/guests.d/rhel-5-*/ks.cfg; do ksvalidator -e -v RHEL5 $f; done
 for f in ${workdir}/guests.d/rhel-6-*/ks.cfg; do ksvalidator -e -v RHEL6 $f; done
-for f in ${workdir}/guests.d/{rhel-7-,jboss}*/ks.cfg; do ksvalidator -e -v RHEL7 $f; done
+for f in ${workdir}/guests.d/{rhel-7-,jboss,satellite6}*/ks.cfg; do ksvalidator -e -v RHEL7 $f; done
 for f in ${workdir}/guests.d/ose*/ks.cfg; do ksvalidator -e -v RHEL6 $f; done
 for f in ${workdir}/guests.d/{rhua,cds}*/ks.cfg; do ksvalidator -e -v RHEL6 $f; done
 
