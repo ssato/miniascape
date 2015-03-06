6e9edb23ec7242cc9641ce60cb894802e5bcfb02
Satoru SATOH <satoru.satoh@gmail.com>
2015-03-06 16:31:38 +0900

    simplify setup.py a lot w/ help of rpmbuild wrapper script

--- setup.py
+++ setup.py
@@ -50,12 +50,6 @@ class SrpmCommand(Command):
     user_options = []
 
     build_stage = "s"
-    cmd_fmt = """rpmbuild -b%(build_stage)s \
-        --define \"_topdir %(rpmdir)s\" \
-        --define \"_sourcedir %(rpmdir)s\" \
-        --define \"_buildroot %(BUILDROOT)s\" \
-        %(rpmspec)s
-    """
 
     def initialize_options(self):
         pass
@@ -68,24 +62,17 @@ class SrpmCommand(Command):
         self.build_rpm()
 
     def build_rpm(self):
-        params = dict()
-
-        params["build_stage"] = self.build_stage
-        rpmdir = params["rpmdir"] = os.path.join(os.path.abspath(os.curdir),
-                                                 "dist")
-        rpmspec = params["rpmspec"] = os.path.join(rpmdir,
-                                                   "../%s.spec" % PACKAGE)
-
-        for subdir in ("SRPMS", "RPMS", "BUILD", "BUILDROOT"):
-            sdir = params[subdir] = os.path.join(rpmdir, subdir)
-
-            if not os.path.exists(sdir):
-                os.makedirs(sdir, 0755)
+        curdir = os.path.abspath(os.curdir)
+        rpmspec = os.path.join(curdir, "%s.spec" % PACKAGE)
 
         c = open(rpmspec + ".in").read()
         open(rpmspec, "w").write(c.replace("@VERSION@", VERSION))
 
-        os.system(self.cmd_fmt % params)
+        rpmbuild = os.path.join(curdir, "pkg/rpmbuild-wrapper.sh")
+        workdir = os.path.join(curdir, "dist")
+
+        os.system("%s -w %s -s %s %s" % (rpmbuild, workdir, self.build_stage,
+                                         rpmspec))
 
 
 class RpmCommand(SrpmCommand):
