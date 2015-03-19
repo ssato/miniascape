eaee7adb92fabdd67c08e3d189afa30f5c2f89ee
Satoru SATOH <satoru.satoh@gmail.com>
2015-03-19 17:47:43 +0900

    cleanups

--- templates/guest/vmbuild.sh
+++ templates/guest/vmbuild.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 # see also virt-install(1)
-#
 function genmac () { python -c 'from random import randint as f; print ":".join("%02x" % x for x in (0x52, 0x54, 0x00, f(0x00, 0x7f), f(0x00, 0xff),  f(0x00, 0xff)))'; }
 {% macro net_option(nic) -%}
 {%     if nic.bridge is defined -%}
@@ -40,60 +39,53 @@ path={{ disk.path }}
 {%     endif -%}
 {% endmacro -%}
 set -ex
-{% block kscfg -%}
 test $# -gt 0 && ks_path=$1 || ks_path=${0%/*}/ks.cfg
 kscfg=${ks_path##*/}
-{%- endblock %}
-name={% if name_prefix is defined %}{{ name_prefix }}{% endif -%}
-{%  if hostname is defined %}{{ hostname }}{% else %}{{ name }}{% endif %}
-connect=${QEMU_CONNECT:-{{ virtinst.connect }}}
-
+name={{ '%s' % name_prefix if name_prefix }}{{ '%s' % hostname or name }}
+connect=${QEMU_CONNECT:-{{ virtinst.connect|default('qemu:///system') }}}
 {% block location -%}
-{% if virtinst.cdrom -%}
-{%     if virtinst.extra_args is defined and virtinst.extra_args -%}
-# --extra-args="{{ virtinst.extra_args }}"  # It does not work w/ --cdrom but ...
-{%     endif -%}
+{%     if virtinst.cdrom -%}
 location_opts="--cdrom {{ virtinst.cdrom }}"
-{% else -%}
+{%     else -%}
 location_opts="--location={{ virtinst.location }} --initrd-inject=${ks_path}"
-more_extra_args="{{ virtinst.extra_args|default('') }}"
-{%     if interfaces|length > 1 -%}
-more_extra_args="$more_extra_args ksdevice={{ ksdevice|default('eth0') }}"
+extra_args="{{ 'inst.loglevel=debug inst.headless inst.text inst.' if virtinst.os_variant and virtinst.os_variant == 'rhel7' }}ks=file:/${kscfg} {{ virtinst.extra_args|default('') }}"
+{%         if interfaces|length > 1 -%}
+extra_args="$extra_args{{ ' %s' % ksdevice if ksdevice }}"
+{%         endif -%}
 {%     endif -%}
-{% endif -%}
-{%- endblock %}
+{% endblock %}
 
 {% block virtio_scsi_def -%}
 # Use virtio-scsi if available and there is a scsi disk:
-virtio_scsi_controller=
-{%-     for disk in disks if disk.bus is defined and disk.bus == 'scsi' -%}
-{%-         if loop.first -%}
-"--controller=scsi,model=virtio-scsi"{% else %}""
-{%-         endif -%}
-{%-     endfor %}
-{%- endblock %}
+{%     for disk in disks if disk.bus and disk.bus == 'scsi' -%}
+{{ 'virtio_scsi_controller="--controller=scsi,model=virtio-scsi"' if loop.first -}}
+{%     endfor -%}
+{% endblock %}
 
 {% block virt_install -%}
 virt-install \
-{{ virtinst.basic_options }} \
+{{     virtinst.basic_options|default('--check-cpu --hvm --accelerate --noreboot --noautoconsole') }} \
 --name=${name:?} \
 --connect=${connect:?} \
---wait={{ virtinst.waittime }} \
---ram={{ virtinst.ram }} \
---arch={{ virtinst.arch }} \
---vcpus={{ virtinst.vcpus }} {% if virtinst.cpu is defined %}--cpu {{ virtinst.cpu }}{% endif %} \
---graphics {{ virtinst.graphics }}{% if keyboard is defined %},keymap={{ keyboard }}{% endif %} \
---os-type={{ virtinst.os_type }} \
+--wait={{ virtinst.waittime|default('10') }} \
+--ram={{ virtinst.ram|default('1024') }} \
+--arch={{ virtinst.arch|default('x86_24') }} \
+--vcpus={{ virtinst.vcpus }} \
+{{     ' --cpu %s' % virtinst.cpu if virtinst.cpu }} \
+--graphics {{ virtinst.graphics|default('vnc') -}}
+{{-    ',keymap=%s' % keyboard if keyboard }} \
+--os-type={{ virtinst.os_type|default('linux') }} \
 --os-variant={{ virtinst.os_variant }} \
-{%	if virtinst.os_variant == 'rhel7' -%}
---console pty,target_type=serial \
-{%	endif -%}
+{%     if virtinst.os_variant and virtinst.os_variant == 'rhel7' -%}
+--console pty,target_type=serial {%	endif -%} \
 ${virtio_scsi_controller} \
-${location_opts} {% if virtinst.cdrom is not defined %}--extra-args="ks=file:/${kscfg} ${more_extra_args}"{% endif %} \
-{%    for disk in disks -%}
+${location_opts} \
+--extra-args="${extra_args}" \
+{%     for disk in disks -%}
 --disk {{ disk_option(disk) }} \
-{%    endfor %} \
-{%    for nic in interfaces -%}
+{%     endfor %} \
+{%     for nic in interfaces -%}
 --network {{ net_option(nic) }} \
-{%    endfor %}
-{%- endblock %}
+{%     endfor %}
+{% endblock %}
+# vim:sw=4:ts=4:et:
