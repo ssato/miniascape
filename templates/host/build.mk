22120387bcd62b53a04a1c346eb2ab6be3bf95e7
Satoru SATOH <satoru.satoh@gmail.com>
2015-03-20 20:29:57 +0900

    add

---
+++ templates/host/build.mk
@@ -0,0 +1,15 @@
+TOPDIR = .
+SITE ?= {{ site|default('default') }}
+
+miniascape_OPTIONS = -t $(TOPDIR)/templates
+
+
+all: build
+
+build:
+	miniascape b $(miniascape_OPTIONS) -C $(TOPDIR)/$(SITE) -w $(TOPDIR)/out
+
+setup:
+	for d in $(wildcard $(TOPDIR)/out/guests.d/*); do test -d $$d && make -C $$d setup || :; done 
+
+.PHONY: build setup
