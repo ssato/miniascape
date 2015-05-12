34670fc90f9505e5871e9e9fc7e743bb74c17547
Satoru SATOH <satoru.satoh@gmail.com>
2015-05-12 21:23:28 +0900

    update the copyright header

--- miniascape/config.py
+++ miniascape/config.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2012 - 2014 Satoru SATOH <ssato at redhat.com>
+# Copyright (C) 2012 - 2015 Satoru SATOH <ssato at redhat.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
