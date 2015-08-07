fa819ff736f645b5b73425cf4532d54f37ba75b7
Satoru SATOH <satoru.satoh@gmail.com>
2015-08-07 16:44:30 +0900

    now OSE uses RHEL7 not RHEL6

--- pkg/apptests.sh
+++ pkg/apptests.sh
@@ -25,7 +25,7 @@ for f in ${workdir}/guests.d/{rhel-7-,jboss,satellite6}*/ks.cfg; do
   sed -i '/^%addon/,/%end/d' $f
   ksvalidator -e -v RHEL7 $f
 done
-for f in ${workdir}/guests.d/ose*/ks.cfg; do ksvalidator -e -v RHEL6 $f; done
+for f in ${workdir}/guests.d/ose*/ks.cfg; do ksvalidator -e -v RHEL7 $f; done
 for f in ${workdir}/guests.d/{rhua,cds}*/ks.cfg; do ksvalidator -e -v RHEL6 $f; done
 
 cleanup
