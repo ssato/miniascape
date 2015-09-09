3d4d0307aac41e864fb20b2a172aebbb2fca96e8
Satoru SATOH <satoru.satoh@gmail.com>
2015-09-09 16:08:39 +0900

    add build target to make ks.cfg iso images for guests

--- templates/host/build.mk
+++ templates/host/build.mk
@@ -9,6 +9,9 @@ all: build
 build:
 	miniascape b $(miniascape_OPTIONS) -C $(TOPDIR)/$(SITE) -w $(TOPDIR)/out
 
+floppy:
+	for d in $(wildcard $(TOPDIR)/out/guests.d/*); do test -d $$d && make -C $$d floppy || :; done
+
 setup:
 	for d in $(wildcard $(TOPDIR)/out/guests.d/*); do test -d $$d && make -C $$d setup || :; done 
 
