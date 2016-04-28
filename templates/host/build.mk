f7384e4f87087b5ff2745f7d44d07a0d7498623f
Satoru SATOH <satoru.satoh@gmail.com>
2016-04-28 11:08:36 +0900

    enhancement: add target to setup doc/

--- templates/host/build.mk
+++ templates/host/build.mk
@@ -3,6 +3,10 @@ SITE ?=
 
 miniascape_OPTIONS = -t $(TOPDIR)/templates
 
+ifeq ($(strip $(SITE)),)
+$(error Variable SITE must be set!)
+endif
+
 
 all: build
 
@@ -15,4 +19,10 @@ floppy:
 setup:
 	for d in $(wildcard $(TOPDIR)/out/guests.d/*); do test -d $$d && make -C $$d setup || :; done 
 
-.PHONY: build setup
+doc:
+	test -d $(TOPDIR)/doc || \
+	docutils-exts-bootstrap -w $(TOPDIR)/doc -C '$(TOPDIR)/$(SITE)/*.yml' \
+		`anyconfig_cli -T --get doc.name '$(TOPDIR)/$(SITE)/*.yml'` \
+		-t `anyconfig_cli -T --get doc.type '$(TOPDIR)/$(SITE)/*.yml'` -D -R
+
+.PHONY: build floppy setup doc
