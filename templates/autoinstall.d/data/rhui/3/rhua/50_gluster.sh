dfe235c32623a7d478134f7a3d644ddec722c32f
Satoru SATOH <satoru.satoh@gmail.com>
2017-06-14 11:24:03 +0900

    fix: s/quorum-type/cluster.server-quorum-type/

--- templates/autoinstall.d/data/rhui/3/rhua/50_gluster.sh
+++ templates/autoinstall.d/data/rhui/3/rhua/50_gluster.sh
@@ -55,9 +55,8 @@ EOC
 
 # Configure Gluster Storage Volume Quorum
 test ${NUM_CDS} -le 2 && \
-_ssh_exec ${CDS_0} 'gluster volume set rhui_content_0 quorum-type auto' || \
-_ssh_exec ${CDS_0} 'gluster volume set rhui_content_0 quorum-type server; \
-gluster volume set all cluster.server-quorum-ratio 51%'
+_ssh_exec ${CDS_0} 'gluster volume set rhui_content_0 cluster.server-quorum-type auto' || \
+_ssh_exec ${CDS_0} 'gluster volume set rhui_content_0 cluster.server-quorum-type server; gluster volume set all cluster.server-quorum-ratio 51%'
 _ssh_exec ${CDS_0} "gluster volume info rhui_content_0"
 
 # vim:sw=4:ts=4:et:
