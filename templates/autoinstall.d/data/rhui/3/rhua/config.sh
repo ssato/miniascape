0b4ca76f6c3c9a3bccee670d2121887f458a2693
Satoru SATOH <satoru.satoh@gmail.com>
2017-06-15 01:16:06 +0900

    fix: correct jinja2 template syntax error

--- templates/autoinstall.d/data/rhui/3/rhua/config.sh
+++ templates/autoinstall.d/data/rhui/3/rhua/config.sh
@@ -66,7 +66,7 @@ RHUI_STORAGE_MOUNT_OPTIONS="{{ rhui.storage.mnt_options|join(',')|default('rw') 
 
 RHUI_INSTALLER_TLS_OPTIONS="--certs-country {{ rhui.tls.country|default('JP') }} --certs-state {{ rhui.tls.state|default('Tokyo') }} --certs-city {{ rhui.tls.city }} --certs-org {{ rhui.tls.org }}"
 {%- if proxy is defined and proxy.fqdn is defined %}
-RHUI_INSTALLER_TLS_OPTIONS="${RHUI_INSTALLER_TLS_OPTIONS:?} --proxy-protocol={{ protocol.protocol|default('http') }} --proxy-hostname={{ proxy.fqdn }} --proxy-port={{ proxy.port|default("443") }}"
+RHUI_INSTALLER_TLS_OPTIONS="${RHUI_INSTALLER_TLS_OPTIONS:?} --proxy-protocol={{ proxy.protocol|default('http') }} --proxy-hostname={{ proxy.fqdn }} --proxy-port={{ proxy.port|default("443") }}"
 {%-    if proxy.user is defined %}
 RHUI_INSTALLER_TLS_OPTIONS="${RHUI_INSTALLER_TLS_OPTIONS:?} --proxy-username={{ proxy.user }} --proxy-password={{ proxy.password }}"
 {%-    endif %}
