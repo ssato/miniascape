5ef78b43f87f255b6de1ba5e5032d45284f98ffb
Satoru SATOH <satoru.satoh@gmail.com>
2017-09-12 12:50:15 +0900

    fix: only generate gpg configs if these do not exist

--- templates/autoinstall.d/data/rhui/3/rhua/gen_rpm_gpgkey.sh
+++ templates/autoinstall.d/data/rhui/3/rhua/gen_rpm_gpgkey.sh
@@ -5,6 +5,7 @@ custom_gpg_key=/root/setup/RPM-GPG-KEY-rhui-custom
 custom_gpg_conf=/root/setup/rhui-gpg.conf
 
 test -d /root/setup || mkdir -p /root/setup
+test -f ${custom_gpg_conf} || (
 cat << EOF > ${custom_gpg_conf}
 Key-Type: RSA
 Key-Length: 1024
@@ -18,15 +19,20 @@ Passphrase: {{ gpg.passpharase }}
 %transient-key
 %commit
 EOF
+)
 
+test -d .gnupg/ || (
 (find / -xdev -type f -exec sha256sum {} >/dev/null \; 2>&1) &
 export ENTROPY_GEN_PROCESS=$!
 gpg -v --batch --gen-key ${custom_gpg_conf} && kill ${ENTROPY_GEN_PROCESS}
+)
 
 keyid=$(gpg --list-keys | sed -nr 's,^pub[^/]+/(.+) [0-9].*,\1,p')
 
+test -f ${custom_gpg_key} || \
 gpg -a --export ${keyid:?} > ${custom_gpg_key}
 
+test -f /root/dot.rpmmacros || (
 cat << EOF > /root/dot.rpmmacros
 %_signature gpg
 %_gpg_name ${keyid:?}
@@ -42,6 +48,7 @@ cat << EOF > /root/dot.rpmmacros
 %_binary_filedigest_algorithm 1
 %_default_patch_fuzz 2
 EOF
+)
 
 (cd /root && test -f .rpmmacros || ln -s dot.rpmmacros .rpmmacros)
 #rpm --import ${custom_gpg_key}
