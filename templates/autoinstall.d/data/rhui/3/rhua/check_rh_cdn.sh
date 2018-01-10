7a122b59d343bd073fa1f35567b455efedcc093e
Satoru SATOH <satoru.satoh@gmail.com>
2018-01-11 01:36:27 +0900

    fix: do not rely on the status code and try to find out '200 OK' from curl's debug output

--- templates/autoinstall.d/data/rhui/3/rhua/check_rh_cdn.sh
+++ templates/autoinstall.d/data/rhui/3/rhua/check_rh_cdn.sh
@@ -5,23 +5,18 @@
 # Author: Satoru SATOH <ssato at redhat.com>
 # License: MIT
 #
-# Usage: $0 [TLS_CLI_CERT [TIMEOUT_SEC [RH_CDN_URL]]]
+# Usage: $0 [TLS_CLI_CERT [TIMEOUT_SEC [RH_CDN_URL [CHECK_KEYWORD]]]]
 #
-# Example:
-#
-# [root@rhui-2 ~]# ./$0
-# 
 set -ex
 
-TLS_CA_CERT=/etc/pki/rhui/ca.crt
+TLS_CA_CERT=/etc/rhsm/ca/redhat-uep.pem
 TLS_CLI_CERT=${1:-$(ls -1 /etc/pki/rhui/redhat/*.pem | head -n 1)}
 TIMEOUT=${2:-5}  # [sec]
 RH_CDN_URL=${3:-https://cdn.redhat.com/content/dist/rhel/rhui/server/7/7Server/x86_64/os/repodata/repomd.xml}
+CHECK_KEYWORD='200 OK'   # primary.xml, ...
 
-timeout ${TIMEOUT:?} \
-curl -v \
---cacert ${TLS_CA_CERT:?} \
---cert ${TLS_CLI_CERT:?} \
-${RH_CDN_URL:?}
+#timeout ${TIMEOUT:?} \
+curl -v --connect-timeout ${TIMEOUT:?} \
+--cacert ${TLS_CA_CERT:?} --cert ${TLS_CLI_CERT:?} ${RH_CDN_URL:?} 2>&1 | grep -q "${CHECK_KEYWORD:?}"
 
 # vim:sw=4:ts=4:et:
