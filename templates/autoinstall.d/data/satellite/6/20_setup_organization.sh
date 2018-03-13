456bf88e967584f31b5cad92e3ddae9148ff9047
Satoru SATOH <satoru.satoh@gmail.com>
2018-03-13 13:23:14 +0900

    enhancement: [satellite] list organization, location and defaults after initial configuration

--- templates/autoinstall.d/data/satellite/6/20_setup_organization.sh
+++ templates/autoinstall.d/data/satellite/6/20_setup_organization.sh
@@ -16,10 +16,12 @@ source ${WORKDIR}/setup_hammer_user_conf.sh
 # Create Organization if not found.
 hammer --csv organization list | grep -qE ",${ORG_LABEL}," 2>/dev/null || \
 hammer organization create --name="${ORG_NAME}" --label="${ORG_LABEL}"
+hammer --output=yaml organization list
 
 # Create Location if not found.
 hammer --csv location list | grep -qE ",${LOC_NAME}," 2>/dev/null || \
 hammer location create --name="${LOC_NAME}"
+hammer --output=yaml location list
 
 org_id=$(hammer --output=yaml organization info --label "${ORG_LABEL}" | sed -nr "s/^Id: //p")
 loc_id=$(hammer --output=yaml location info --name "${LOC_NAME}" | sed -nr "s/^Id: //p")
@@ -31,6 +33,7 @@ echo ${org_id} > ${ORG_ID_FILE:?}.t && mv ${ORG_ID_FILE}.t ${ORG_ID_FILE}
 
 # see: Satellite 6.2 Hammer CLI Guide, 1.4. Setting a Default Organization:
 # http://red.ht/2Dv8A2x
+hammer defaults list
 hammer defaults add --param-name organization_id --param-value ${org_id}
 hammer defaults add --param-name location_id --param-value ${loc_id}
 hammer defaults list
