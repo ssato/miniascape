ce8c4b9f42b89a7eb19004ac566efb3ac5e49ae7
Satoru SATOH <satoru.satoh@gmail.com>
2018-03-14 03:49:23 +0900

    fix: [satellite] use repository(-set) name if available instead of repo.name

--- templates/autoinstall.d/data/satellite/6/config.sh
+++ templates/autoinstall.d/data/satellite/6/config.sh
@@ -102,14 +102,24 @@ tweak_selinux_policy () { :; }
 
 ENABLE_YUM_REPOS_FOR_CLIENTS="
 {% for repo in satellite.repos if repo.name -%}
-hammer repository-set enable --name '{{ repo.name }}' --product '{{ repo.product|default('Red Hat Enterprise Linux Server') }}' {{ '--basearch %s' % repo.arch|default('x86_64') }} {{ '--releasever %s' % repo.releasever if repo.releasever is defined and repo.releasever }} || :
+hammer repository-set enable --name '{{ repo.set_name or repo.name }}' --product '{{ repo.product|default('Red Hat Enterprise Linux Server') }}' {{ '--basearch %s' % repo.arch|default('x86_64') }} {{ '--releasever %s' % repo.releasever if repo.releasever is defined and repo.releasever }} || :
 {% endfor -%}
 "
+
 PRODUCTS="
 {% for p in satellite.products if p.name -%}
 {{      p.name }}
 {% endfor -%}
 "
+
+#PRODUCTS_TO_SYNC="
+#{% for p in satellite.products
+#       if p.name and p.download_policy and p.download_policy != 'on_demand' -%}
+#for rid in $(hammer --output=yaml repository list --product '{{ p.name }}' | sed -nr 's/ID: (.*)/\1/p'); do
+#   hammer repository update --download-policy '{{ p.download_policy }}' --id ${rid:?} --product '{{ p.name }}'
+#done
+#{% endfor -%}
+#"
 
 LIST_REPOSITORY_SET_BY_PRODUCTS="
 {% for p in satellite.products if p.name -%}
